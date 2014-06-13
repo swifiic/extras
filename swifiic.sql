3e46e192bce2c2888f5ccd2eba499283d966822c
Shivam Khandelwal <shivamchinu@gmail.com>
2014-06-13 17:42:47 +0530

    Modified Users table to add profile pictures column.

--- swifiic.sql
+++ swifiic.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.0.6deb1
+-- version 4.2.3
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Apr 11, 2014 at 01:51 PM
--- Server version: 5.5.35-0ubuntu0.13.10.2
--- PHP Version: 5.5.3-1ubuntu2.3
+-- Generation Time: Jun 13, 2014 at 05:41 PM
+-- Server version: 5.5.37-MariaDB-log
+-- PHP Version: 5.5.13
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -27,19 +27,10 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE IF NOT EXISTS `Operators` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+`id` int(11) NOT NULL,
   `username` varchar(32) NOT NULL,
-  `password` varchar(32) NOT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `username` (`username`)
+  `password` varchar(32) NOT NULL
 ) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=4 ;
-
---
--- Dumping data for table `Operators`
---
-
-INSERT INTO `Operators` (`id`, `username`, `password`) VALUES
-(3, 'shivam', 'password');
 
 -- --------------------------------------------------------
 
@@ -48,24 +39,43 @@ INSERT INTO `Operators` (`id`, `username`, `password`) VALUES
 --
 
 CREATE TABLE IF NOT EXISTS `Users` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+`id` int(11) NOT NULL,
   `username` varchar(32) NOT NULL,
   `alias` varchar(32) NOT NULL,
   `dtn_id` text NOT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `username` (`username`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=8 ;
+  `profile_pic` blob NOT NULL
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=12 ;
 
 --
--- Dumping data for table `Users`
+-- Indexes for dumped tables
 --
 
-INSERT INTO `Users` (`id`, `username`, `alias`, `dtn_id`) VALUES
-(1, 'shivam', 'Shivam', 'dtn://shivam-nexus'),
-(2, 'abhishek', 'Abhishek', 'dtn://at-motog'),
-(6, 'test', 'Test', 'dtn://test-device'),
-(7, 'aniket', 'Aniket', 'dtn://aniket-motog');
+--
+-- Indexes for table `Operators`
+--
+ALTER TABLE `Operators`
+ ADD PRIMARY KEY (`id`), ADD UNIQUE KEY `username` (`username`);
 
+--
+-- Indexes for table `Users`
+--
+ALTER TABLE `Users`
+ ADD PRIMARY KEY (`id`), ADD UNIQUE KEY `username` (`username`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `Operators`
+--
+ALTER TABLE `Operators`
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=4;
+--
+-- AUTO_INCREMENT for table `Users`
+--
+ALTER TABLE `Users`
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=12;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
